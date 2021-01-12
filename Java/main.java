1b2a8160be145c3062db4b2c02780f0635442a8c
LeeIngyu <DevelopLee20@gmail.com>
2021-01-12 11:28:44 +0900

    Add Dart Folder

--- Java/main.java
+++ Java/main.java
@@ -1,7 +1,7 @@
 public class main{
     public static void main(String[] args){
         double dnum = 3.14;
-        float fnum = 3.14f;
+        float fnum = 3.14;
 
         System.out.println(dnum);
         System.out.println(fnum);
