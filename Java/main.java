4682d02c80fd8296f6b14d9f5437d6995b6822ad
lkjono02 <lkjoon02@naver.com>
2021-02-06 21:41:39 +0900

    first PR

--- Java/main.java
+++ Java/main.java
@@ -1,9 +1,5 @@
 public class main{
     public static void main(String[] args){
-        double dnum = 3.14;
-        float fnum = 3.14;
-
-        System.out.println(dnum);
-        System.out.println(fnum);
+        
     }
 }
